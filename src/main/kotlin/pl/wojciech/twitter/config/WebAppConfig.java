826a9aa6b434b0e706a177e7de1d37f2b07fad74
Wojciech <wojciech.gladalski@gmail.com>
2019-12-01 21:27:57 +0100

    small repairment

--- src/main/kotlin/pl/wojciech/twitter/config/WebAppConfig.java
+++
@@ -1,8 +0,0 @@
-package pl.wojciech.twitter.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebAppConfig implements WebMvcConfigurer {
-}
