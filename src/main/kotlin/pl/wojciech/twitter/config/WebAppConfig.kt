94a1205fe93461d0283adfa25ed84482e6cae623
Wojciech <wojciech.gladalski@gmail.com>
2019-12-01 21:17:38 +0100

    converted to kotlin file

---
+++ src/main/kotlin/pl/wojciech/twitter/config/WebAppConfig.kt
@@ -0,0 +1,9 @@
+package pl.wojciech.twitter.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebAppConfig : WebMvcConfigurer{
+
+}
