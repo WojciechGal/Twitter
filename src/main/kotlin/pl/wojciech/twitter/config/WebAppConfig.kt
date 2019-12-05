8a962e65ddf49bc4ac82b4c86699cabaae7bad85
Wojciech <wojciech.gladalski@gmail.com>
2019-12-05 22:04:48 +0100

    added view resolver

--- src/main/kotlin/pl/wojciech/twitter/config/WebAppConfig.kt
+++ src/main/kotlin/pl/wojciech/twitter/config/WebAppConfig.kt
@@ -1,9 +1,13 @@
 package pl.wojciech.twitter.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebAppConfig : WebMvcConfigurer{
 
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/login").setViewName("login")
+    }
 }
