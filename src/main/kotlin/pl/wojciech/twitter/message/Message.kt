a72f96374a8d41eecdf81038635694ff15b0aece
Wojciech <wojciech.gladalski@gmail.com>
2019-12-14 12:42:25 +0100

    entity for messages

---
+++ src/main/kotlin/pl/wojciech/twitter/message/Message.kt
@@ -0,0 +1,31 @@
+package pl.wojciech.twitter.message
+
+import pl.wojciech.twitter.author.Author
+import java.time.LocalDateTime
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+@Entity
+@Table(name = "messages")
+class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var Id: Long? = null
+
+    @NotNull
+    @ManyToOne
+    var author: Author? = null
+
+    @Size(min = 3)
+    var text: String? = null
+
+    @Column(name = "created_on")
+    var createdOn: LocalDateTime? = null
+
+    @PrePersist
+    fun prePersist() {
+        createdOn = LocalDateTime.now()
+    }
+}
